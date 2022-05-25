bcb3e0d649d07297f09a2416430416e489b439bd
Abhishek <abhishekc838@gmail.com>
2022-05-25 23:02:30 +0530

    start db connection

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB.");
+    } catch (error) {
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected!");
+})
+
+app.get('/abhi', (req, res) => {
+    res.send("hello Jai Shree Raam")
+})
+
+app.listen(8000, () => { 
+    connect()
+    console.log("Connected to server...")
+})
