46f39d8abd256ea4020f4f2f14dfcdfd04e97ba5
Abhishek <abhishekc838@gmail.com>
2022-06-02 21:37:04 +0530

    routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+
 const app = express();
 dotenv.config()
 
@@ -21,9 +27,11 @@ mongoose.connection.on("connected", () => {
     console.log("mongoDB connected!");
 })
 
-app.get('/abhi', (req, res) => {
-    res.send("hello Jai Shree Raam")
-})
+//middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8000, () => { 
     connect()
